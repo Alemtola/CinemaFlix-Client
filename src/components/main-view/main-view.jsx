63374bf9bdfead73ba1be1590b79b6d382e06688
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-20 14:25:54 +0200

    updated the component for more movie info

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -12,17 +12,23 @@ export class MainView extends React.Component {
       movies: [
         { _id: 1,
           Title: 'Inception',
-          Description: 'desc1...',
+          Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
+          Genre: 'Sci-Fi',
+          Director: 'Christopher Nolan',
           ImagePath: 'inception.jpg'
         },
         { _id: 2,
           Title: 'The Shawshank Redemption',
-          Description: 'desc2...',
+          Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+          Genre: 'Drama',
+          Director: 'Frank Darabont',
           ImagePath: 'shawshank_redemption_ver2.jpg'
         },
         { _id: 3,
           Title: 'Gladiator',
-          Description: 'desc3...',
+          Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
+          Genre: 'Adventure',
+          Director: 'Ridley Scott',
           ImagePath: 'gladiator_ver1.jpg'
         }
       ],
