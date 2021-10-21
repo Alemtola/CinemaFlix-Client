5eb4c5054fe84c513eb77de1cab24d8783e71611
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-21 22:29:35 +0200

    added registration-view component

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import PropTypes from "prop-types";
+
+export function RegistrationView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ birthday, setBirthday] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+    /* Send a request to the server for authentication */
+    /* then call props on registored user(username) */
+    props.onRegistration(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Birthday:
+        <input type="date" value={Birthday} onChange={e => setBirthday(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
+
+RegistrationView.propTypes = {
+  onRegistration: PropTypes.func.isRequired,
+};
