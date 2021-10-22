7dc7f88cf5bf3ae83e0a4df75bb511a819230677
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-22 13:02:31 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -5,7 +5,7 @@ export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ email, setEmail ] = useState('');
-  const [ birthday, setBirthday] = useState('');
+  const [ Birthday, setBirthday] = useState('');
 
 
   const handleSubmit = (e) => {
