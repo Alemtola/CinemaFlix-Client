06673ed0c262809c2e00039d8eafb6ec285bbf6d
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-22 18:06:18 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -10,7 +10,7 @@ export function RegistrationView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password, email, birthday);
+    console.log(username, password, email, Birthday);
     /* Send a request to the server for authentication */
     /* then call props on registored user(username) */
     props.onRegistration(username);
