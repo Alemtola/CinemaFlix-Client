4142069419d09a61d29ef63805a90beabaf921d9
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-25 17:29:37 +0200

    applied bootstrap card for the movie cards

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick  } = this.props;
 
     return (
-      <div className="movie-card" onClick={() => { onMovieClick(movie); }}>
-          {movie.Title}
-      </div>,
-    )
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.ReleaseYear}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
+    ReleaseYear: PropTypes.number.isRequired,
     Description: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired
