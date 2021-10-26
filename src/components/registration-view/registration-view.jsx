c75f51f02ef58322e27a57f6f54e486419882da2
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-26 18:05:47 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from "prop-types";
+import { Form, Button, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
+
+
+import './registration-view.scss';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -17,25 +21,46 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthday:
-        <input type="date" value={Birthday} onChange={e => setBirthday(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Container>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card className="registerCard">
+              <Card.Body>
+                <Card.Title className="text-center">Welcome to CinemaFlix.</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted text-center">Please Register</Card.Subtitle>
+            
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Birthday</Form.Label>
+                    <Form.Control className="mb-3" type="date" value={Birthday} onChange={e => setBirthday(e.target.value)} />
+                  </Form.Group>
+                   
+                  <Button className="registerButton" variant="secondary" size="lg" type="submit" onClick={handleSubmit}>Register</Button>
+                  
+                </Form>
+              </Card.Body>
+            </Card>
+         </CardGroup>
+        </Col>
+      </Row>
+    </Container>
+    
   );
 }
 
