4bd99da5c90a9e5eedaf76b67e5fc29483bbe247
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-28 21:04:07 +0200

    update movie view structure and styling

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 import './movie-view.scss';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -21,29 +22,37 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} crossOrigin="true" />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
 
-       </div>
+      <Container fluid className="moviesContainer">
+        <Row>
+          <Col>
+            <div className="movie-view">
+              <div className="movie-poster">
+                <img src={movie.ImagePath} crossOrigin="true" />
+              </div>
+              <div className="movie-title">
+                <span className="title">Title: </span>
+                <span className="value">{movie.Title}</span>
+              </div>
+              <div className="movie-description">
+                <span className="description">Description: </span>
+                <span className="value">{movie.Description}</span>
+              </div>
+              <div className="movie-genre">
+                <span className="genre">Genre: </span>
+                <span className="value">{movie.Genre.Name}</span>
+              </div>
+              <div className="movie-director">
+                <span className="director">Director: </span>
+                <span className="value">{movie.Director.Name}</span>
+              </div>
+              <button onClick={() => { onBackClick(null); }}>Back</button>
+
+            </div>
+          </Col>
+        </Row>
+        
+       </Container>
     );
   }
 }
