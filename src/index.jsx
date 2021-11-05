44b0d713af42e4e950410c833bd6e4b8a930c5f1
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-11-05 11:45:35 +0100

    added redux DevTools

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import { MainView } from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
 //Import statement indicating need to bundle `./index.scss`
 import './index.scss';
 
-const store = createStore(moviesApp);
+const store = createStore(moviesApp, devToolsEnhancer());
 
 //The Main component
 class MyFlixApplication extends React.Component {
