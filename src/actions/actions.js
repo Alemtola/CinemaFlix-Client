690adccd540ec1f151ac9198f746fc5992009aa2
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-11-05 17:23:20 +0100

    added update user  action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,7 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
-
+export const UPDATE_USER = "UPDATE_USER";
 
 export function setMovies(value) {
   return { 
@@ -17,10 +17,17 @@ export function setFilter(value) {
   };
 }
 
-export function setUser(user) {
+export function setUser(value) {
   return { 
     type: SET_USER, 
-    user
+    value
+  };
+}
+
+export function updateUser(value) {
+  return {
+    type: UPDATE_USER,
+    value,
   };
 }
 
